cffa3da60128e3c6da7dea77eabf0d55842c04e1
Anna <annapapros21@gmail.com>
2021-10-05 02:43:16 +0200

    sign up form

--- components/SignUpPage/SignUpPage.tsx
+++ components/SignUpPage/SignUpPage.tsx
@@ -1,44 +1,62 @@
 import React from "react";
 import Image from "next/image";
-import { makeStyles } from "@mui/styles";
+import { createStyles, makeStyles } from "@mui/styles";
 import start from "../../public/img/signup.jpg";
 import Logo from "../Logo";
+import { TextField, Theme, Typography } from "@mui/material";
+import RectangularButton from "../RectangularButton";
+import UnderlinedLink from "../UnderlinedLink";
+import { ROUTES } from "../../config/configNav";
 
-const useStyles = makeStyles({
-  root: {
-    textAlign: "center",
-  },
-  img: {
-    position: "relative",
-    height: "100vh",
-    maxWidth: "100vw",
-    "&::after": {
-      content: "''",
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      backgroundImage:
-        "linear-gradient(90deg, rgba(255,255,255,0.37) 0%, rgba(255,255,255,1) 60%)",
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      textAlign: "center",
     },
-  },
-  banner: {
-    position: "relative",
-    height: "100vh",
-    width: "100%",
-  },
-  contentBanner: {
-    position: "absolute",
-    right: 0,
-    top: 0,
-    width: "50%",
-    height: " 50%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+    img: {
+      position: "relative",
+      height: "100vh",
+      maxWidth: "100vw",
+      "&::after": {
+        content: "''",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundImage:
+          "linear-gradient(90deg, rgba(255,255,255,0.37) 0%, rgba(255,255,255,1) 60%)",
+      },
+    },
+    banner: {
+      position: "relative",
+      height: "100vh",
+      width: "100%",
+    },
+    contentBanner: {
+      position: "absolute",
+      right: 0,
+      top: 0,
+      width: "50%",
+      height: "80%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    form: {
+      display: "flex",
+      flexDirection: "column",
+      textAlign: "center",
+      marginTop: theme.spacing(3),
+    },
+    button: {
+      textAlign: "center",
+      margin: theme.spacing(3),
+      textTransform: "none",
+    },
+  })
+);
 
 export default function SignUpPage() {
   const classes = useStyles();
@@ -54,7 +72,45 @@ export default function SignUpPage() {
         />
       </div>
       <div className={classes.contentBanner}>
-        <Logo variantLogo="h4" />
+        <Logo variantLogo="h3" />
+        <div className={classes.form}>
+          <TextField
+            id="login"
+            label="E-mail"
+            size="small"
+            variant="outlined"
+            margin="normal"
+            type="email"
+          />
+          <TextField
+            id="password"
+            label="Hasło"
+            size="small"
+            variant="outlined"
+            margin="normal"
+            type="password"
+          />
+          <TextField
+            id="password"
+            label="Powtórz hasło"
+            size="small"
+            variant="outlined"
+            margin="normal"
+            type="password"
+          />
+          <RectangularButton
+            color="primary"
+            variant="outlined"
+            size="medium"
+            className={classes.button}
+          >
+            Zarejestruj się
+          </RectangularButton>
+          <Typography color="GrayText" variant="body2">
+            Posiadasz już konto?
+          </Typography>
+          <UnderlinedLink route={ROUTES.signInClient} />
+        </div>
       </div>
     </div>
   );
