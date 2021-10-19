85a9accfd63f4d664f1defbcf125c57442cf138a
Anna <annapapros21@gmail.com>
2021-10-19 14:32:09 +0200

    locations grid

---
+++ pages/companies-locations-list.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import Layout from "../components/common/Layout";
+import { CompaniesLocationsListPage } from "../components/CompaniesLocationsListPage";
+
+const CompaniesLocationslist: NextPage = () => {
+  return <CompaniesLocationsListPage />;
+};
+
+export default CompaniesLocationslist;
