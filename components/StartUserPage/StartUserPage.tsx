b2c857cc8b95fefacab377692e1744a058ed0c65
Anna <annapapros21@gmail.com>
2021-10-22 18:30:02 +0200

    test disable calendar

--- components/StartUserPage/StartUserPage.tsx
+++ components/StartUserPage/StartUserPage.tsx
@@ -20,7 +20,6 @@ export default function StartUserPage() {
   return (
     <div>
       <Banner />
-      <CalendarSection />
       <TasksSection />
       <BudgetSection />
       <GuestSection />
