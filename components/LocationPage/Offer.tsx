5bce58ce27aa6cb38d5c4f0a0f7e0c457e9cf4dd
Anna <annapapros21@gmail.com>
2021-11-06 08:13:25 +0100

    draft js test

--- components/LocationPage/Offer.tsx
+++ components/LocationPage/Offer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import { createStyles, makeStyles } from "@mui/styles";
 import startCompanies from "../../public/img/startCompanies.jpg";
@@ -10,11 +10,17 @@ import RectangularButton from "../RectangularButton";
 import UnderlinedLink from "../UnderlinedLink";
 import DecorationTypography from "../DecorationTypography";
 import { Service } from "../../config/types";
+import { EditorState, Editor, RichUtils, convertToRaw } from "draft-js";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     content: {
       textAlign: "center",
+      padding: theme.spacing(5, 0),
+    },
+    editor: {
+      height: "100%",
+      border: "solid thin " + theme.palette.primary.main,
       minHeight: "300px",
     },
   })
@@ -28,5 +34,33 @@ export default function Offer({
   mode?: "view" | "edit";
 }) {
   const classes = useStyles();
-  return <div className={classes.content}>{service.offer}</div>;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const editor = useRef(null);
+
+  const handleKeyCommand = (command: string) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      setEditorState(newState);
+      return "handled";
+    }
+    return "not-handled";
+  };
+
+  function focusEditor() {
+    //@ts-ignore
+    editor.current.focus();
+  }
+  console.log(convertToRaw(editorState.getCurrentContent()).blocks);
+  return (
+    <div className={classes.content}>
+      <div className={classes.editor} onClick={focusEditor}>
+        <Editor
+          ref={editor}
+          editorState={editorState}
+          onChange={setEditorState}
+          handleKeyCommand={handleKeyCommand}
+        />
+      </div>
+    </div>
+  );
 }
