1025a99037f43b170ca754e9b352dda36c2c3c06
Anna Papros <53228201+Idelya@users.noreply.github.com>
2022-07-26 22:51:28 +0200

    Update settings.ts

--- config/settings.ts
+++ config/settings.ts
@@ -1,6 +1,6 @@
 const SETTINGS = {
   server: process.env.NEXT_PUBLIC_BACKEND_URL,
-  apikey: "43t3$H#V)0U83vnru#V)(NRU)#VRnu930)(URVN#(U@*YRV&@YR&",
+  apikey: process.env.NEXT_PUBLIC_API_KEY,
   cloud_name: process.env.NEXT_PUBLIC_CLOUD_NAME,
   cloud_link: process.env.NEXT_PUBLIC_CLOUD_LINK,
   upload_preset: process.env.NEXT_PUBLIC_IMAGE_UPLOAD,
